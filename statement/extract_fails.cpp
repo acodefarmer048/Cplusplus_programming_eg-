290f3c5c0a0f0e0814a90222ad045d0c4feabde9
wellington <2200016621@stu.pku.edu.cn>
2023-07-28 23:04:36 +0800

    update extract_fail.cpp code

--- statement/extract_fails.cpp
+++ statement/extract_fails.cpp
@@ -1,21 +1,19 @@
 #include <list>
+#include <algorithm>
 #include <iostream>
 #include "grade.h"
 #include "Student_info.h"
 #include "extract_fails.h"
-using std::list;
+using std::list; using std::stable_partition;
+
+bool pgrade(const Student_info& s){
+    return !fgrade(s);
+}
 list<Student_info> extract_fails(list<Student_info>& students){
-    list<Student_info> fail;
-    list<Student_info>::iterator iter=students.begin();
-    while (iter!=students.end())
-    {
-        if(fgrade(*iter)){
-            fail.push_back(*iter);
-            iter = students.erase(iter);
-        }else{
-            ++iter;
-        }
-    }
+    list<Student_info>::iterator iter=
+        stable_partition(students.begin(), students.end(), pgrade);
+    list<Student_info> fail(iter, students.end());
+    students.erase(iter, students.end())
     return fail;
     
 }
